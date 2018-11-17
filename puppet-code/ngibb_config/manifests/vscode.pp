3f86fab3e4bb43a134b19958e378012ca51bc11c
Nolan Gibb <nolan.gibb@smartsheet.com>
2018-11-16 16:39:01 -0800

    Added a key for vscode

--- puppet-code/ngibb_config/manifests/vscode.pp
+++ puppet-code/ngibb_config/manifests/vscode.pp
@@ -1,28 +1,32 @@
-# ngibb_config::vscode
+# config::vscode
 # code --help
 
 class ngibb_config::vscode {
-
+ 
+  # configure source for package
   apt::source {'vscode':
-    comment      => 'microsoft',
+    comment      => 'vscode',
     architecture => 'amd64',
     location     => 'https://packages.microsoft.com/repos/vscode',
     release      => 'stable',
     repos        => 'main',
- #   key          => {
- #     'id '    => 'BC528686B50D79E339D3721CEB3E94ADBE1229CF',
- #     'source' => 'https://packages.microsoft.com/keys/microsoft.asc',
- #   }
+    key          => {
+      id     => 'BC528686B50D79E339D3721CEB3E94ADBE1229CF',
+      source => 'https://packages.microsoft.com/keys/microsoft.asc',
+    }
   }
   
+  # install packages
   package {"code":
     ensure => "present"
   }
 
+  # set up config dir
   file {"/home/${config_user}/.vscode":
     ensure => "directory",
   }
 
+  # set up extension dir
   file {"/home/${config_user}/.vscode/extensions":
     ensure => "directory",
     require => File["/home/${config_user}/.vscode"],
@@ -35,9 +39,7 @@ class ngibb_config::vscode {
     unless   => "code --list-extensions --show-versions | grep -q 'vscodevim'",
     cwd      => "/tmp/",
     path     => "/bin/:/usr/bin:/user/bin/code",
-    require  => File["/home/${config_user}/.vscode/extensions"],
+    require  => [File["/home/${config_user}/.vscode/extensions"], Package["code"]]
   }
 
-
 }
-
