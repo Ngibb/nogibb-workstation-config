c9aebd0b769955b3429910168d8bc0c5926ca8c5
Nolan Gibb <nolan.gibb@smartsheet.com>
2019-02-04 11:16:22 -0800

    Add docker-ce install

--- puppet_code/desktop_config/manifests/apps/docker.pp
+++ puppet_code/desktop_config/manifests/apps/docker.pp
@@ -1,22 +1,23 @@
 # desktop_config::apps::docker
 # Docker is straight broken on my computer
 
-class desktop_config::apps::dock {
+class desktop_config::apps::docker {
 
   apt::source {'docker':
     comment => 'docker', 
+    architecture => 'amd64',
     location => 'https://download.docker.com/linux/debian/',
     release => 'stretch',
     repos => 'stable',
-    include => {
-      'deb' => true,
-      'src' => true,
+    key => {
+      id     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+      source => 'https://download.docker.com/linux/debian/gpg',
     },
   }
 
   # Is this even right? 
   package { "docker-ce":
-    ensure => installed
+   ensure => installed
   }
 
 }
