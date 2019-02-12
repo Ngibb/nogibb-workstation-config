ba0000a917679fd2c0ed5508314c482b3d190acc
Nolan Gibb <nolan.gibb@smartsheet.com>
2019-02-12 09:18:45 -0800

    Add emulators

---
+++ puppet_code/desktop_config/manifests/apps/emulators.pp
@@ -0,0 +1,19 @@
+# desktop_config::apps::emulators
+
+class desktop_config::apps::emulators {
+
+  # nes
+  package { "zsnes":
+    ensure => installed
+  }
+
+  # gba
+  package { "visualboyadvance":
+    ensure => installed
+  }
+  package { "visualboyadvance-gtk":
+    ensure => installed
+  }
+
+}
+
