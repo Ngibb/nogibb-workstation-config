7931b73c346666cc055876e5bc626f9e30fa562d
Nolan Gibb <nolan.gibb@smartsheet.com>
2019-05-13 10:23:53 -0700

    -q for wget

--- puppet_code/desktop_config/manifests/apps/discord.pp
+++ puppet_code/desktop_config/manifests/apps/discord.pp
@@ -3,7 +3,7 @@
 class desktop_config::apps::discord {
 
         exec {"install_discord":
-          command => "wget 'https://discordapp.com/api/download?platform=linux&format=deb' -O /tmp/discord.deb && dpkg -i /tmp/discord.deb",
+          command => "wget -q 'https://discordapp.com/api/download?platform=linux&format=deb' -O /tmp/discord.deb && dpkg -i /tmp/discord.deb",
           unless => "dpkg -l  | grep -iq 'discord'",
           cwd => "/tmp/",
           path => "/sbin/:/bin/:/usr/sbin:/usr/bin:/usr/local/bin", 
