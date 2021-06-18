d0965072beae54183e609419cafd2f270b39d941
Nolan Gibb <ngibb94@gmail.com>
2021-06-17 17:58:44 -0700

    Prune some crustly stuff, learn :sort

--- configure_me.pp
+++ configure_me.pp
@@ -1,30 +1,15 @@
-# Configure my computer!
-
-# Vars
 # User who this is being configed for
-$config_user = 'ngibb'
+$config_user = lookup(config_user, String)
+
 $config_user_home_dir = "/home/${config_user}"
 $zshrc_dir = "${config_user_home_dir}/.zshrc.d"
 $bashrc_dir = "${config_user_home_dir}/.bashrc.d"
 # where this git repo lives
 $workstation_config_dir = "/home/${config_user}/git/workstation-config"
-# puppet code from the git repo, there will be link from /etc/puppet dir
-$desktop_config_dir = "${workstation_config_dir}/puppet_code/desktop_config"
-# puppet module proper directory
-$puppet_code_dir = "/etc/puppet/code/modules/"
-
-# Using the hostname to ID what kind of computer this is
-$host_type = $facts['hostname'] ? {
-  'cuiaba'	     => 'workstation',
-  'debian-vm'	   => 'workstation',
-  'campo-grande' => 'homestation',
-  'fatima'       => 'laptop',
-  'default'      => '', 
-}
 
 # default behavior for the file file type
 File {
-  backup => ".backup",
+  backup => '.backup',
   owner => $config_user,
   group => $config_user,
   show_diff => true,
@@ -32,40 +17,40 @@ File {
 
 $wanted_packages = [
 	'aptitude',
-	'zsh',
-	'keepass2',
-	'keepassx',
+	'bless', # hex editor
+	'cgdb',
+	'cryptsetup',
+	'curl',
+	'ddd', # debugger
+	'deluge',
 	'dnsutils',
-	'geoip-bin',
-	'libfdisk1',
+	'dos2unix',
 	'gcc',
 	'gdb',
-	'cgdb',
-	'mlocate',
-	'dos2unix',
-	'openconnect',
-	'mtr',
-	'strace',
-	'wireshark',
-	'nmap',
-	'iperf',
-	'deluge',
+	'geoip-bin',
+	'imagemagick',
 	'iotop',
-	'curl',
+	'iperf',
+	'jq',
+	'keepass2',
+	'keepassx',
+	'libfdisk1',
+	'libreoffice',
+	'mlocate',
+	'mtr',
+	'multitail',
+	'netdiag',
+	'nmap',
+	'openconnect',
+	'rsync',
+	'strace',
+	'unzip',
 	'vlc',
 	'whois',
-	'cryptsetup',
-	'netdiag',
-	'rsync',
-	'imagemagick',
-	'jq',
-	'ddd', # debugger
-	'libreoffice',
-	'unzip',
-	'multitail',
-	'bless', # hex editor
-        'htop',
-        'packer', # aws image creation 
+	'wireshark',
+	'zsh',
+  'htop',
+  'packer', # aws image creation 
 ]
 
 # Packages I want / are useful
@@ -75,7 +60,7 @@ $wanted_packages.each |String $my_package|{
 	}
 }
 
-$unwanted_games = [
+$unwanted_packages = [
 	'aisleriot',
 	'five-or-more',
 	'four-in-a-row',
@@ -96,11 +81,12 @@ $unwanted_games = [
 	'swell-foop',
 	'tali'
 ]
+
 # Packages I don't want
-$unwanted_games.each |String $not_my_package|{
-	package { $not_my_package:
-		ensure => absent 
-	}
+$unwanted_packages.each |String $not_my_package|{
+  package { $not_my_package:
+    ensure => absent
+  }
 }
 
 user { $config_user:
@@ -109,33 +95,35 @@ user { $config_user:
   require => "Package[zsh]",
 }
 
-include "desktop_config::${host_type}"
-#include desktop_config::motd
-
+# Essentials
 include desktop_config::debian_sources
-
 include desktop_config::zsh_setup
 include desktop_config::bash_setup
 
-include desktop_config::apps::spotify
-include desktop_config::apps::virtualbox
-include desktop_config::apps::discord
-include desktop_config::apps::chrome
-include desktop_config::apps::terraform
-include desktop_config::apps::aws_okta_cli
+#Tools
 include desktop_config::apps::aws_es_proxy
+include desktop_config::apps::aws_okta_cli
+include desktop_config::apps::chrome
+include desktop_config::apps::docker
+include desktop_config::apps::virtualbox
+include desktop_config::apps::terraform
 include desktop_config::apps::slack
 include desktop_config::apps::irssi
-include desktop_config::apps::terminator
 include desktop_config::apps::vim
 include desktop_config::apps::git
 include desktop_config::apps::burp
 include desktop_config::apps::gron
-include desktop_config::apps::docker
 include desktop_config::apps::eclipse
 include desktop_config::apps::vscode
 include desktop_config::apps::signal
 include desktop_config::apps::obs_studio
-include desktop_config::apps::system_tray_icons
-include desktop_config::apps::yubioath_desktop
 include desktop_config::apps::surfshark
+include desktop_config::apps::system_tray_icons
+include desktop_config::apps::terminator
+include desktop_config::apps::yubioath_desktop
+
+# Fun :)
+include desktop_config::apps::spotify
+if lookup(install_games, Boolean, 'first', false ) {
+  include desktop_config::games
+}
--- puppet_code/desktop_config/manifests/games.pp
+++ puppet_code/desktop_config/manifests/games.pp
@@ -1,12 +1,11 @@
 # desktop_config_homestation
-class desktop_config::homestation {
-  include desktop_config::not_workstation
+class desktop_config::games {
   include desktop_config::apps::steam
 	include desktop_config::apps::discord
   include desktop_config::apps::emulators
  
   # Xbox Controller drivers
-  package {"xboxdrv":
+  package {'xboxdrv':
     ensure => installed
   } 
 }
