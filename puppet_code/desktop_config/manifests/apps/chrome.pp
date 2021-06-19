11eda0a716427e6dc3e5bcc52d517e6678a846a5
Nolan Gibb <ngibb94@gmail.com>
2021-06-18 19:48:39 -0700

    Refactor google chrome

--- puppet_code/desktop_config/manifests/apps/chrome.pp
+++ puppet_code/desktop_config/manifests/apps/chrome.pp
@@ -23,23 +23,27 @@ class desktop_config::apps::chrome {
 
 
   # kerberos
-  file {"/etc/opt/chrome/policies":
-    ensure  => 'directory',
-    owner   => root,
-    group   => root,
-  } 
-  file {"/etc/opt/chrome/policies/managed":
-    ensure  => 'directory',
-    owner   => root,
-    group   => root,
-  } 
-  file {"/etc/opt/chrome/policies/managed/kerb.json":
-    ensure  => 'file',
-    source  => ["puppet:///modules/desktop_config/google-chrome/kerb.json",],
-    owner   => root,
-    group   => root,
-    require => Package["google-chrome-stable"] 
-  }
+  $auth_server_whitelist = lookup('chrome.kerbconfig.auth_server_whitelist')#, undef, undef, false)
+  if $auth_server_whitelist {
+    file {'/etc/opt/chrome/policies':
+      ensure => 'directory',
+      owner  => root,
+      group  => root,
+    }
 
+    file {'/etc/opt/chrome/policies/managed':
+      ensure => 'directory',
+      owner  => root,
+      group  => root,
+    }
+
+    file {'/etc/opt/chrome/policies/managed/kerb.json':
+      ensure  => 'file',
+      content => template("${module_name}/google-chrome/kerb.json.erb"),
+      owner   => root,
+      group   => root,
+      require => Package['google-chrome-stable']
+    }
+  }
 }
 
