730870baf3febbe0a28ce5cf012e50538a61ecfb
Nolan Gibb <ngibb94@gmail.com>
2021-06-19 19:12:23 -0700

    The great purge

--- puppet_code/desktop_config/manifests/apps/aws_okta_cli.pp
+++ puppet_code/desktop_config/manifests/apps/aws_okta_cli.pp
@@ -2,50 +2,45 @@
 
 class desktop_config::apps::aws_okta_cli {
 
-  apt::source { "segment_aws-okta":
-    comment  => "for the repository at https://packagecloud.io/segment/aws-okta",
+  apt::source { 'segment_aws-okta':
+    comment  => 'for the repository at https://packagecloud.io/segment/aws-okta',
     location => 'https://packagecloud.io/segment/aws-okta/debian/', 
     repos    => 'main',
     release  => 'stretch',
     include  => {
-     'src' => true,
-     'deb' => true,
+      'src' => true,
+      'deb' => true,
     },
     key      => {
       'id'     => 'B446BAEA8B4D2AA43A3568AF0D90FFB9DBCA25A4',
-      'source'     => 'https://packagecloud.io/segment/aws-okta/gpgkey',
+      'source' => 'https://packagecloud.io/segment/aws-okta/gpgkey',
     },
-  } -> 
+  }
 
   # Need to update before trying to install aws-okta
-  Class['apt::update'] ->
+  -> Class['apt::update']
 
-	package{"aws-okta":
-	  ensure => "present",
-	}
+  -> package{'aws-okta':
+    ensure => 'present',
+  }
 
-	package{"awscli":
-	  ensure => "present",
-	}
+  package{'awscli':
+    ensure => 'present',
+  }
 
-	$aws_config_dir = "/home/${config_user}/.aws"
+  $aws_config_dir = "/home/${config_user}/.aws"
 
-	file{"aws_config_dir":
-	  ensure => "directory",
+  file{'aws_config_dir':
+    ensure => 'directory',
     path   => $aws_config_dir ,
-	}
-
-	file{"${aws_config_dir}/config":
-    content => file("${module_name}/aws_okta/aws_okta_config"),
-    require => File["aws_config_dir"],
-	}
+  }
 
 # export AWS_OKTA_BACKEND=file
-	file{"${zshrc_dir}/aws_okta.zsh":
+  file{"${zshrc_dir}/aws_okta.zsh":
     content => file("${module_name}/aws_okta/aws_okta_rc"),
-	}
-	file{"${bashrc_dir}/aws_okta.sh":
+  }
+  file{"${bashrc_dir}/aws_okta.sh":
     content => file("${module_name}/aws_okta/aws_okta_rc"),
-	}
+  }
 
 }
