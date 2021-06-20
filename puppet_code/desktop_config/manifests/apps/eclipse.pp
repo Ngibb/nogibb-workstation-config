cb605aa199f23755c7585afa5bc26a371899fca9
Nolan Gibb <ngibb94@gmail.com>
2021-06-19 17:22:43 -0700

    eclipse a little better

--- puppet_code/desktop_config/manifests/apps/eclipse.pp
+++ puppet_code/desktop_config/manifests/apps/eclipse.pp
@@ -2,22 +2,25 @@
 # TODO: Install other plugins
 
 class desktop_config::apps::eclipse (
-  $install_dir = "/opt",
+  $install_dir = '/opt',
   $eclipse_dir = "${install_dir}/eclipse",
+  $eclispe_version = lookup('eclipse.version', undef, undef, 2018-12)
 ){
 
-  $download_uri="https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2018-12/R/eclipse-java-2018-12-R-linux-gtk-x86_64.tar.gz&r=1"
- 
+  # From https://www.eclipse.org/downloads/packages/
+  $download_uri="https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/${eclispe_version}/R/eclipse-java-${eclispe_version}-R-linux-gtk-x86_64.tar.gz&r=1"
+
   # I guess "aptitude install" would work, but seems *old*
-  exec {"install_eclipse":
+  exec {'install_eclipse':
     command => "wget '${download_uri}' -O /tmp/eclipse.tar.gz && tar xzf eclipse.tar.gz -C ${install_dir}",
-    unless => "test -e ${eclipse_dir}",
-    cwd => "/tmp/",
-    path => "/sbin/:/bin/:/usr/sbin:/usr/bin:/usr/local/bin", 
+    unless  => "test -e ${eclipse_dir}",
+    cwd     => '/tmp/',
+    path    => '/sbin/:/bin/:/usr/sbin:/usr/bin:/usr/local/bin', 
   }
 
+
+  # Set bash alias to open eclipse
   $eclipse_exec_path="${eclipse_dir}/eclipse"
-  # eclipse path
   file{"${zshrc_dir}/eclipse.zsh":
     content => "# open eclipse \n alias eclipse=\"${eclipse_exec_path}\"",
   }
