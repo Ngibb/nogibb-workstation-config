415e31ec6b323456048120fa7fbce3fd92a04671
Nolan Gibb <ngibb@gmail.com>
2022-03-29 20:36:51 -0700

    Add a script to help with running puppet the firsst time

--- helper_scripts/prepare_pupppet.sh
+++ helper_scripts/prepare_pupppet.sh
@@ -5,6 +5,10 @@ sudo apt-get install -y curl wget git
 echo ""
 
 
+
+
+
+
 PUPPET_VERSION=7
 DEBIAN_RELEASE=buster
 PUPPET_APT_URL=https://apt.puppet.com
@@ -16,6 +20,11 @@ PUPPET_BINARY_PATH=/opt/puppetlabs/bin/puppet
 echo "Install Puppet version ${PUPPET_VERSION} for ${DEBIAN_RELEASE} from $PUPPET_DOWNLOAD_URL"
 curl -o $PUPPET_CURL_FILE_PATH $PUPPET_DOWNLOAD_URL
 echo ""
+
+
+# Create SSH Key
+# Prompt upload of SSH Key
+# Download the Git repo
 
 
 echo "Install the puppet release package"
@@ -36,7 +45,25 @@ echo ""
 
 set -xe
 
-CONFIG_DIR_PATH=$(dirname $0)
+# Create SSH Key
+cd ~/.ssh
+ssh-keygen -t ecdsa
+cd ~
+# Prompt upload of SSH Key
+echo "Upload this ssh key"
+cat ~/.ssh/id_ecdsa.pub
+echo ""
+echo "Add the key https://gitlab.com/-/profile/keys"
+echo ""
+echo "Press any key to continue"
+read  -n 1 -s 
+echo ""
+
+# Download the Git repo
+git clone git@gitlab.com:Ngibb/workstation-config.git
+git clone git@gitlab.com:Ngibb/workstation-config-hiera.git
+
+CONFIG_DIR_PATH="/home/ngibb/workstation-config"
 echo "Run puppet"
 echo "sudo $PUPPET_BINARY_PATH apply ${CONFIG_DIR_PATH}/configure_me.pp -t --noop --modulepath=${CONFIG_DIR_PATH}/puppet_code:/etc/puppetlabs/code/environments/production/modules"
 echo ""
