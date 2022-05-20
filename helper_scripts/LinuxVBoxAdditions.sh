8f9d10d3ddcb79e83d40683971251318fdf7e119
Nolan Gibb <ngibb94@gmail.com>
2022-05-20 04:29:44 +0000

    [fix] vbox script uses su and installs vim.

--- helper_scripts/LinuxVBoxAdditions.sh
+++ helper_scripts/LinuxVBoxAdditions.sh
@@ -1,8 +1,9 @@
-su -c "/usr/sbin/usermod -a -G sudo ngibb"
+su -c "apt-get install -y vim"
+su -c "/usr/sbin/usermod -a -G sudo ${USER}"
 
-sudo apt install dkms linux-headers-$(uname -r) build-essential
-sudo mount /dev/cdrom /mnt
+su -c "apt install dkms linux-headers-$(uname -r) build-essential"
+su -c "mount /dev/cdrom /mnt"
 
-sudo sh /mnt/VBoxLinuxAdditions.run
+su -c "sh /mnt/VBoxLinuxAdditions.run"
 
-echo "Make sure ot reboot and give the VM more resources"
+echo "Make sure to reboot and give the VM more resources. Think will also take care of logout/login for sudo."
